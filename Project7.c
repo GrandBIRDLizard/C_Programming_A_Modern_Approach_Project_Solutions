cf94805e3891d00879310f6012901c03ed266bca
GrandBIRDLizard <justin.munch9@gmail.com>
2025-05-15 18:25:03 -0400

    StyleUpdate.c

--- Project7.c
+++ Project7.c
@@ -7,11 +7,15 @@ int main(void)
 	scanf("%d", &dollars);
 
 	printf("Lowest number of bills due:\n");
+	
 	printf ("$20's: %d\n", dollars / 20);
 	dollars = (dollars % 20);
+	
 	printf("$10's: %d\n", dollars / 10);
 	dollars = (dollars % 10);
+	
 	printf("$5's: %d\n", dollars / 5);
 	dollars = (dollars % 5);
+	
 	printf("$1's: %d\n", dollars / 1);
 }
