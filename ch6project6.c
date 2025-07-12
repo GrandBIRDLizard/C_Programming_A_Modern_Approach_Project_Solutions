0d180c1c3700f53b44ba699f3230f9f9e2470a18
GrandBIRDLizard <justin.munch9@gmail.com>
2025-07-12 11:52:01 -0400

    Update ch6project6.c

--- ch6project6.c
+++ ch6project6.c
@@ -4,7 +4,7 @@ int main()
 {
 	int n, i;
 
-	printf("Enter number to find all squares within: ");
+	printf("Enter number to find all even squares within: ");
 	scanf("%d", &n);
 	
 	for (i = 2; i * i <= n; i += 2) {
