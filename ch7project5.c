73872e93074cb67578045c3363f6fae0e24d06ba
GrandBIRDLizard <justin.munch9@gmail.com>
2025-07-20 18:49:22 -0400

    Add files via upload

---
+++ ch7project5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char op;
+
+	int sum = 0;
+	printf("Enter a word: ");
+	
+	while ((op = getchar()) != '\n') {
+		op = toupper(op);
+		switch (op) {
+			case 'A': case 'E': case 'I': case 'L': case 'N': case 'O': case 'R': case 'S':case 'T': case 'U':
+				sum += 1;
+				break;
+			case 'D': case 'G':
+				sum += 2;
+				break;
+			case 'B': case 'C': case 'M': case 'P':
+				sum += 3;
+				break;
+			case 'F': case 'H': case 'V': case 'W': case 'Y':
+				sum += 4;
+				break;
+			case 'K': 
+				sum += 5;
+				break;
+			case 'J': case 'X': 
+				sum += 8;
+				break;
+			case 'Q': case 'Z':
+				sum += 10;
+				break;
+			default:
+				printf("please enter a valid word");
+		}
+		
+	}
+	printf("Scrabble value: %d", sum);
+
+	return 0;
+}
